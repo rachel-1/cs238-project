cdf708340cd7c11129e1ec728b5120bc08b77b20
Jihyeon Janel Lee <janel@cs.stanford.edu>
2019-10-27 21:03:51 -0700

    initializing graph with nodes for each busid_stopid pai

--- graph_setup.py
+++ graph_setup.py
@@ -1,5 +1,6 @@
-import networkx as nx
 import json
+import math
+import networkx as nx
 
 
 def add_test1_data(G):
@@ -19,23 +20,48 @@ def add_test1_data(G):
     G.add_node('end', x=4, y=0)
 
 
-def init_grid_from_latlon_bounds(transit_graph,
+def init_grid_from_latlon_bounds(G,
+                                 transit_graph,
                                  lat_start=37.675016,
                                  lat_end=37.814703,
                                  lon_start=-122.505734,
                                  lon_end=-122.385306):
     # Default params taken from SF bounding box
     # https://github.com/sisl/MultiAgentAllocationTransit.jl
-    # Rows = Lat = y, Cols = Long = x
-    # Learn how large cells can be using average differences
 
-    return
+    # Each cell size is step degrees wide and step degrees tall
+    # Rows = Lat = y, Cols = Lon = x
+    step = 0.001  # degrees
+    num_rows = int(math.ceil(abs(lat_end - lat_start) / float(step)))
+    num_cols = int(math.ceil(abs(lon_end - lon_start) / float(step)))
+
+    get_row = lambda lat : int(round((lat - lat_start) / float(step)))
+    get_col = lambda lon : int(round((lon - lon_start) / float(step)))
+
+    # Assume drone starts top left
+    G.add_node('current', x=0, y=0)
+    # Destination bottom right
+    G.add_node('end', x=num_cols-1, y=num_rows-1)
+
+    # Each node is named bus_id_stop_id
+    # Attributes: bus_id, stop_id, estimated_arrival_time,
+    # x (col), y (row), lat, lon
+    for bus_id, bus_stops in enumerate(transit_graph['transit_trips']):
+        for stop in bus_stops:
+            stop_id = stop['stop_id']
+            stop_coords = transit_graph['stop_to_location'][str(stop_id)]
+            row = get_row(stop_coords['lat'])
+            col = get_col(stop_coords['lon'])
+
+            node_id = str(bus_id) + '_' + str(stop_id)
+            G.add_node(node_id, bus_id=bus_id, stop_id=stop_id, x=col,
+                       y=row, estimated_arrival_time=stop['arrival_time'])
 
 
 def add_sf_oct_2019_data(G, transit_graph_file='transit_graph_Oct_2019.json'):
     with open(transit_graph_file, 'r') as f:
-        data = json.load(f)
-
+        transit_graph = json.load(f)
+    init_grid_from_latlon_bounds(G, transit_graph)
     return
 
 
@@ -44,5 +70,7 @@ def init_graph(graph_name):
 
     if graph_name == 'test1':
         add_test1_data(G)
+    elif graph_name  == 'sf_oct_2019':
+        add_sf_oct_2019_data(G)
 
     return G
