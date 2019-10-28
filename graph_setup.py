073c6bb608de9941db816b90eb10ff84b44ef4ab
Rachel Gardner <rachelgardner9@gmail.com>
2019-10-27 17:05:47 -0700

    Basic graph solving is working

--- graph_setup.py
+++ graph_setup.py
@@ -8,7 +8,7 @@ def add_test1_data(G):
     G.add_node(1, estimated_arrival_time=1, x=1, y=0)
 
     # bus 1 (stop 2)
-    G.add_node(2, estimate_arrival_time=2, x=3, y=0)
+    G.add_node(2, estimated_arrival_time=2, x=3, y=0)
 
     # bus 1 will always go between stops (riding edge)
     G.add_edge(1,2)
