5afa5534e130c36cb5570955c393aa3934a87194
Jihyeon Janel Lee <janel@cs.stanford.edu>
2019-11-12 14:03:13 -0800

    adding index of each stop with respect to this bus route

--- graph_setup.py
+++ graph_setup.py
@@ -14,7 +14,7 @@ def add_test1_data(G):
     G.add_node(2, estimated_arrival_time=2, x=3, y=0)
 
     # bus 1 will always go between stops (riding edge)
-    G.add_weighted_edge([(1,2,1)])
+    G.add_weighted_edge([(1, 2, 1)])
 
     # drone goal
     G.add_node('end', x=4, y=0)
@@ -32,37 +32,52 @@ def init_grid_from_latlon_bounds(G,
     # Each cell size is step degrees wide and step degrees tall
     # Rows = Lat = y, Cols = Lon = x
     step = 0.001  # degrees
+    # Given step of 0.001 deg,
+    # Height and width of each cell is ~ 110 m, hypotenuse is 155 m
+    # 140 columns, 120 rows
     num_rows = int(math.ceil(abs(lat_end - lat_start) / float(step)))
     num_cols = int(math.ceil(abs(lon_end - lon_start) / float(step)))
 
-    get_row = lambda lat : int(round((lat - lat_start) / float(step)))
-    get_col = lambda lon : int(round((lon - lon_start) / float(step)))
+    def get_row(lat): return int(round((lat - lat_start) / float(step)))
+    def get_col(lon): return int(round((lon - lon_start) / float(step)))
 
     # Assume drone starts top left
     G.add_node('current', x=0, y=0)
     # Destination bottom right
-    G.add_node('end', x=num_cols-1, y=num_rows-1)
+    G.add_node('end', x=num_cols - 1, y=num_rows - 1)
 
     # Each node is named bus_id_stop_id
-    # Attributes: bus_id, stop_id, estimated_arrival_time,
-    # x (col), y (row), lat, lon
+    # Attributes:
+    #   bus_id,
+    #   stop_id,
+    #   stop_idx - index of stop on this route,
+    #   estimated_arrival_time,
+    #   x - column within grid,
+    #   y - row within grid,
+    #   lat - coords of stop,
+    #   lon
     for bus_id, bus_stops in enumerate(transit_graph['transit_trips']):
-        for stop in bus_stops:
+        for i, stop in bus_stops:
             stop_id = stop['stop_id']
+            stop_idx = i  # the number of this stop on this bus route
+
             stop_coords = transit_graph['stop_to_location'][str(stop_id)]
-            row = get_row(stop_coords['lat'])
+            row = get_row(stop_coords['lat'])  # position in grid
             col = get_col(stop_coords['lon'])
+
+            arrival_time = stop['arrival_time']
 
             node_id = str(bus_id) + '_' + str(stop_id)
             G.add_node(node_id, bus_id=bus_id, stop_id=stop_id, x=col,
-                       y=row, estimated_arrival_time=stop['arrival_time'])
+                       y=row, lat=stop_coords['lat'], lon=stop_coords['lon'],
+                       estimated_arrival_time=arrival_time)
 
 
 def add_sf_oct_2019_data(G, transit_graph_file='transit_graph_Oct_2019.json'):
     with open(transit_graph_file, 'r') as f:
         transit_graph = json.load(f)
+    print(transit_graph)
     init_grid_from_latlon_bounds(G, transit_graph)
-    return
 
 
 def init_graph(graph_name):
@@ -70,7 +85,7 @@ def init_graph(graph_name):
 
     if graph_name == 'test1':
         add_test1_data(G)
-    elif graph_name  == 'sf_oct_2019':
+    elif graph_name == 'sf_oct_2019':
         add_sf_oct_2019_data(G)
 
     return G
